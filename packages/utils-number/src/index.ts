51760c7bee71750cd11aba98cefa792652857fd8
Yogesh Kumar <yogeshkumar0@gmail.com>
2023-02-04 16:38:25 +0000

    removed log

--- packages/utils-number/src/index.ts
+++ packages/utils-number/src/index.ts
@@ -3,9 +3,6 @@ import { add, substract } from './math';
 const x = 20;
 const y = 10;
 
-console.log(`--->>${x} + ${y} = ${add(x, y)}`)
-
-
 export const mySubtract = () => { 
     console.log(`${x} - ${y} = ${substract(x, y)}`);
 }

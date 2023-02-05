106e2455a36ddd71f369639ce215c7fc81f54026
Yogesh Kumar <yogeshkumar0@gmail.com>
2023-02-05 21:53:40 +0000

    update

--- packages/utils-number/src/index.ts
+++ packages/utils-number/src/index.ts
@@ -4,5 +4,5 @@ const x = 20;
 const y = 10;
 
 export const mySubtract = () => { 
-    console.log(`----2${x} - ${y} = ${substract(x, y)}`);
+    console.log(`----3${x} - ${y} = ${substract(x, y)}`);
 }
